083b5287e00b5b01578e39d924afb69070ded5e9
Zhao <94562159+joshuazh8@users.noreply.github.com>
2023-01-24 09:11:20 -0800

    Update app.py

--- tkinter gui csp ap/app.py
+++ tkinter gui csp ap/app.py
@@ -1,5 +1,7 @@
+//added an image opener using pyIMG
 from Tkinter import*
 import PyPDF2
+import PyIMG2
 from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfile
 
@@ -16,7 +18,7 @@ logo_label.image = logo
 logo_label.grid(column=1, row=0)
 
 #instructions
-instructions = tk.Label(root, text="Open a PDF document and extract its text", font="Calibri")
+instructions = tk.Label(root, text="Open a PDF document or an image to extract its content", font="Calibri")
 instructions.grid(columnspan=3, column=0, row=1)
 
 def open_file():
@@ -25,6 +27,12 @@ def open_file():
     if file:
         read_pdf = PyPDF2.PdfFileReader(file)
         page = read_pdf.getPage(0)
+        page_content = page.extractText()
+    browse_text.set("wait...")
+    file = askopenfile(parent=root, mode='rb', title="Choose a file", filetypes=[("Image File", "*.jpg")])
+    if file:
+        read_image = PyIMG2.ImgFileReader(file)
+        page = read_img.getPage(0)
         page_content = page.extractText()
 
         #text box
@@ -41,6 +49,12 @@ browse_text = tk.StringVar()
 browse_btn = tk.Button(root, textvariable=browse_text, command=lambda:open_file(), font="Calibri", bg="#20bebe", fg="white", height=2, width=15)
 browse_text.set("Click Here to Open File")
 browse_btn.grid(column=1, row=2)
+
+#browseImage button
+browseImage_text = tk.StringVar()
+browseImage_btn = tk.Button(root, textvariable=browse_text, command=lambda:open_file(), font="Calibri", bg="#20bebe", fg="white", height=2, width=15)
+browseImage_text.set("Click Here to Open File")
+browseImage_btn.grid(column=1, row=2)
 
 canvas = tk.Canvas(root, width=600, height=250)
 canvas.grid(columnspan=3)
